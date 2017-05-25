3af39f777088b135cf95adc90573a8607717c306
marioiovanna <marioiovanna@hotmail.com>
2017-05-25 00:15:40 -0400

    not java

---
+++ assets/css-java/java.js
@@ -0,0 +1,49 @@
+
+
+var words = [
+	['a','d','v','e','n','t','u','r','e',__,'i','s','l','a','n','d']
+	['a','l','o','n','e',__,'i','n',__,'t','h','e',__,'d','a','r','k']
+	['c','o','n','t','r','a']
+	['m','o','n','k','e','y',__,'i','s','l','a','n','d']
+	
+	];
+
+var win = 0;
+var loss = 0;
+var guesses = [];
+
+
+document.onkeyup = function(event){
+
+	var userword = even.key;
+
+
+	if (event = 1) {
+		computerguess = words.
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(userword)
+
+}
+
